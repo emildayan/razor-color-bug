80470a6efcd8c29fd17cb4661be01a061f864cdc
emildayan <emil@emildayan.se>
2021-08-18 21:00:46 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+const {fontFamily} = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './resources/**/*.blade.php',
+        './vendor/razorui/blade-application-ui/resources/views/components/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+            fontFamily: {
+                sans: ['Inter', ...fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
